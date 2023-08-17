aa9415cae3006018cc86c382a1f9f2668aa41b62
mandibchaulagain <mandibchaulagain8@gmail.com>
2023-08-17 07:32:41 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Settings from './Components/Settings';
+import './App.css';
+
+function App() {
+  return (
+   <div className="App">
+      <h1>Quiz App</h1>
+      <Settings/>
+   </div>
+  );
+}
+
+export default App;
